9b9ad4a12db6d51d1bbddb0f702fd5d005ad60d8
[pouash] <[qartalsh@gmail.com]>
2023-05-15 22:31:23 +0430

    first commit

---
+++ home/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+def home(request:HttpRequest):
+    return render(request,'main.html')
+
+
+
+
+def header_component(request:HttpRequest):
+    context = {}
+    return render(request,'layout/header_component.html',context)
+
+
+def footer_component(request:HttpRequest):
+    context = {}
+    return render(request,'layout/footer_component.html',context)
